fb4036abb402fb513d8e6e9e382d345cebca7c6b
nick <nickbrinser@gmail.com>
2019-08-26 17:14:28 -0500

    Started Monsters-Rolodex

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {CardList} from './components/cardList/card-list.component';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <input 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={e => this.setState({searchField: e.target.value})}
+        
+        />
+        <CardList monsters={this.state.monsters}/>      
+      </div>
+    );
+  }
+}
+
+export default App;
